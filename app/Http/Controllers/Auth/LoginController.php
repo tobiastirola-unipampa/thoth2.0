1c71d7f7b7ea35ba6eec10c122a264c816592711
matt-balda <mateusbalda89@gmail.com>
2023-05-09 16:46:03 -0300

    partial login method for LoginController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,40 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    /**
+    * Faz a autenticação do usuário.
+    */
+    public function login(Request $request)
+    {
+        // validate chamado para validar os campos email e password no objeto $request
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+        
+        // autenticar o usuário com as credenciais fornecidas
+        if (Auth::attempt($credentials)) {
+            // nova sessão 
+            $request->session()->regenerate();
+            // busca o usuário correspondente no banco de dados
+            $user = User::where('email', $request->email)->first();
+            // armazena os dados do usuário na sessão
+            $request->session()->put([
+                'name' => $user->name,
+                'email' => $user->email,
+                'logged_in' => true,
+                'level' => null
+            ]);
+            // registro de log
+            $activity = "Logged in";
+            $this->insertLog($activity, 2, null);
+            // se bem sucedida, rediciona para dashboard
+            return redirect()->intended('/dashboard');
+        }
+        // autenticação falhar, rediciona para login com mensagem de falha 
+        return back()->withErrors([
+            'email' => 'As credenciais informadas não foram encontradas em nossos registros.',
+        ]);
+    }
 }
